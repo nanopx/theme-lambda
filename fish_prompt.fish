e210c28952b1aba94b582c216528d7cbe03a53b1
nanopx <0nanopx@gmail.com>
2018-10-04 15:47:58 +0900

    Fix errors on initialization

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -49,6 +49,27 @@ function fish_prompt
   set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_showstashstate true
   set -g __fish_git_prompt_show_informative_status true 
+
+  function __fish_git_prompt_set_char
+    set -l user_variable_name "$argv[1]"
+    set -l char $argv[2]
+    set -l user_variable
+    if set -q $user_variable_name
+      set user_variable $$user_variable_name
+    end
+
+    if set -q argv[3]
+      and set -q __fish_git_prompt_show_informative_status
+      set char $argv[3]
+    end
+
+    set -l variable _$user_variable_name
+    set -l variable_done "$variable"_done
+
+    if not set -q $variable
+      set -g $variable (set -q $user_variable_name; and echo $user_variable; or echo $char)
+    end
+  end
   
   # __fish_git_prompt_set_char __fish_git_prompt_char_cleanstate '✔'
   __fish_git_prompt_set_char __fish_git_prompt_char_dirtystate '*' '＋'
